4b1d7197f6fc5bced21f5374819dab6dc60623fa
Carles <taradell1966@gmail.com>
2026-08-21 19:28:31 +0200

    fix: service worker network first per evitar cache

--- sw.js
+++ sw.js
@@ -21,5 +21,13 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const clone = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return r;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
